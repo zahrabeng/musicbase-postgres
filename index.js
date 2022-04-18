78b63792f93c593b3c94c64f92bb912481ff8e6f
Zahra <bghzahra@outlook.com>
2022-04-18 16:03:48 +0100

    added client and ascync function not working

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const {Client} = require('pg');
 require ('dotenv').config();
+
+const client = new Client ({
+    user:"academy",
+    password: process.env.password,
+    database:"musicbase"
+});
+
+async function excecute(){
+    await client.connect();
+    const res = client.query("SELECT * FROM artists");
+    console.log(res.rows);
+    await client.end();
+}
+
+excecute()
