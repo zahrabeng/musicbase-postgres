167404a3b082da6eff8a51a2a62f7895e004fdf6
Zahra <bghzahra@outlook.com>
2022-04-18 16:56:54 +0100

    added result rows

--- 1-select-params.js
+++ 1-select-params.js
@@ -3,5 +3,12 @@ const client = new Client ({database:"musicbase"});
 
 async function excecute(){
     await client.connect();
+    const text = "SELECT * FROM artists WHERE id = $1";
+    const values = [3];
+
+    const res = await client.query(text,values)
+    console.log(res.rows)
+    await client.end()
 }
 
+excecute()
