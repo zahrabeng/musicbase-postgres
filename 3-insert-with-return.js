13014c3e5be68bce8005b1655725cf33549b2b3b
Zahra <bghzahra@outlook.com>
2022-04-19 13:17:05 +0100

    successfully adds another artist to local database

--- 3-insert-with-return.js
+++ 3-insert-with-return.js
@@ -4,5 +4,14 @@ const client = new Client ({
 });
 
 async function excecute (){
-    
+    await client.connect();
+    const text = "INSERT INTO artists (id, name) VALUES ($1, $2) RETURNING *";
+    const values = [7, "Justin Timberlake"];
+
+    const res = await client.query(text, values);
+    console.log(res.rows)
+
+    await client.end()
 }
+
+excecute()
